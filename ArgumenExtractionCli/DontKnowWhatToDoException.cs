110be644e72ebe18cfc699aac8aab7e17f75c7d1
Martin Schmidt <inw@immernurwollen.de>
2019-07-25 19:05:39 +0200

    implements a simple CLI

---
+++ ArgumenExtractionCli/DontKnowWhatToDoException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Inw.ArgumentExtractionCli
+{
+    [Serializable]
+    internal class DontKnowWhatToDoException : Exception
+    {
+        public DontKnowWhatToDoException()
+        {
+        }
+
+        public DontKnowWhatToDoException(string message) : base(message)
+        {
+        }
+
+        public DontKnowWhatToDoException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected DontKnowWhatToDoException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ ArgumenExtractionCli/Program.cs
@@ -0,0 +1,78 @@
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Inw.Logger;
+using Microsoft.CodeAnalysis;
+using Inw.ArgumentExtraction.DTO;
+using Inw.ArgumentExtraction.Extractors;
+using Inw.ArgumentExtraction.Finder;
+using Inw.ArgumentExtraction.Loader;
+using System;
+using System.Collections.Generic;
+
+namespace Inw.ArgumentExtractionCli
+{
+    public class Program
+    {
+        private static IDoLog _logger;
+
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                var options = new ProgramOptions(args);
+                if (options.HelpPrinted)
+                    return;
+                
+                await RunAnalysis(options);                
+            }
+            catch (InvalidOperationException ex)
+            {
+                var logger = new ConsoleLogger();
+                if (ex.Message != null)
+                    logger.Error(ex.Message);
+            }
+
+        }
+
+        private static async Task RunAnalysis(ProgramOptions options)
+        {
+            _logger = new ConsoleLogger(verbose: options.VerboseLogging, debug: options.DebugLogging);
+            using (var solLoader = new SolutionLoader(_logger))
+            {
+                var solution = await solLoader.LoadSolution(options.PathToSolution);
+
+                var extractor = new SymbolExtractorWithCompilation(_logger);
+                var declaredSymbols = await extractor.FindSymbols(solution, options.FullTypeName, options.MethodName, options.MethodArguments);
+                _logger.Info($"Did not find anything for {options.FullTypeName}.{options.MethodName}({string.Join(", ", options.MethodArguments)})");
+                await ArgumentExtraction(declaredSymbols, solution, options);
+            }
+        }
+
+        private async static Task ArgumentExtraction(IEnumerable<ISymbol> declaredSymbols, Solution solution, ProgramOptions options)
+        {
+            var argumentExtractor = new InvocationArgumentExtractor(_logger);           
+
+            foreach (var symbol in declaredSymbols)
+            {
+                var argumentResults = await argumentExtractor.FindArguments(symbol, solution);
+                _logger.Info("Reporting location and arguments for calls to " + symbol.ToDisplayString());
+                foreach (var result in argumentResults.OrderBy(r => r.Location.Document.FilePath))
+                {
+                    PrintResult(result);
+                }
+            }
+
+        }
+
+        private static void PrintResult(ArgumentResults result)
+        {
+            var file = result.Location.Document.FilePath;
+            var line = result.Location.Location.GetLineSpan().StartLinePosition;
+
+            var arguments = result.Arguments.Select(a => a.ToString());
+            _logger.Info($"Invocation in {file} at {line} using arguments:");
+            _logger.Info(string.Join(" ; ", arguments));
+        }
+    }
+}
