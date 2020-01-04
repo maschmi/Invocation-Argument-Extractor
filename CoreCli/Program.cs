41dee4d5523ed4ecfec274d51db97782029d1e0e
Martin Schmidt <inw@immernurwollen.de>
2020-01-04 19:00:58 +0100

    set testproject to netcoreapp2.2

--- CoreCli/Program.cs
+++ CoreCli/Program.cs
@@ -39,15 +39,16 @@ namespace Inw.ArgumentExtractionCli
         {
             _logger = new ConsoleLogger(verbose: options.VerboseLogging, debug: options.DebugLogging);
 
-            using var solLoader = new SolutionLoader(new WorkspaceCreator(logger: _logger), _logger);
-            
-            var solution = await solLoader.LoadSolution(options.PathToSolution);
+            using (var solLoader = new SolutionLoader(new WorkspaceCreator(logger: _logger), _logger))
+            {
+                var solution = await solLoader.LoadSolution(options.PathToSolution);
 
-            var extractor = new SymbolExtractorWithCompilation(_logger);
-            var declaredSymbols = await extractor.FindSymbols(solution, options.FullTypeName, options.MethodName, options.MethodArguments);
-            if(declaredSymbols.Count() == 0)
-                _logger.Info($"Did not find anything for {options.FullTypeName}.{options.MethodName}({string.Join(", ", options.MethodArguments)})");
-            await ArgumentExtraction(declaredSymbols, solution, options);
+                var extractor = new SymbolExtractorWithCompilation(_logger);
+                var declaredSymbols = await extractor.FindSymbols(solution, options.FullTypeName, options.MethodName, options.MethodArguments);
+                if(declaredSymbols.Count() == 0)
+                    _logger.Info($"Did not find anything for {options.FullTypeName}.{options.MethodName}({string.Join(", ", options.MethodArguments)})");
+                await ArgumentExtraction(declaredSymbols, solution, options);    
+            }
         }
 
         private static async Task ArgumentExtraction(IEnumerable<ISymbol> declaredSymbols, Solution solution, ProgramOptions options)
